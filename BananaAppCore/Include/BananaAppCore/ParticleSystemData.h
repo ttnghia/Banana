6351c1c85f57197fbfbf156afe38b2b71bfd5bac
Nghia Truong <nghiatruong.vn@gmail.com>
2017-01-21 15:15:20 -0700

    bk

--- BananaAppCore/Include/BananaAppCore/ParticleSystemData.h
+++ BananaAppCore/Include/BananaAppCore/ParticleSystemData.h
@@ -31,9 +31,22 @@
 class ParticleSystemData
 {
 public:
+    enum class DataType
+    {
+        IntType,
+        UIntType,
+        FloatType,
+        DoubleType
+    };
+
+    union ScalarValue
+    {
+        double doubleValue;
+        unsigned int uintValue;
+    };
+
     ParticleSystemData() :
         m_NumParticles(0),
-        m_NumActiveParticles(0),
         m_MaxNumParticles(0)
     {}
 
@@ -44,51 +57,105 @@ public:
 
     void clearData()
     {
-        for(auto it = m_DataArrays.begin(); it != m_DataArrays.end(); ++it)
+        for(auto it = m_ArrayData.begin(); it != m_ArrayData.end(); ++it)
         {
             it->second.clear();
         }
 
-        m_DataArrays.clear();
+        m_ArrayData.clear();
 
         m_NumParticles       = 0;
-        m_NumActiveParticles = 0;
         m_MaxNumParticles    = 0;
     }
 
+    void reserve(unsigned int maxNumParticles)
+    {
+        m_MaxNumParticles    = maxNumParticles;
+
+        for(auto it = m_ArrayData.begin(); it != m_ArrayData.end(); ++it)
+        {
+            const std::string& arrName = it->first;
+            size_t arraySize = m_MaxNumParticles * m_ArrayElementSize[arrName];
+            it->second.resize(arraySize);
+        }
+    }
+
+    bool hasScalar(std::string dataName)
+    {
+        return (m_ScalarData.find(dataName) != m_ScalarData.end());
+    }
+
+    bool hasArray(std::string dataName)
+    {
+        return (m_ArrayData.find(dataName) != m_ArrayData.end());
+    }
+
+    //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    template<class T, int N>
+    void addArray(std::string arrName)
+    {
+        size_t arraySize = m_MaxNumParticles * sizeof(T) * N;
+
+        if(!hasArray(arrName))
+        {
+            m_ArrayElementSize[arrName] = sizeof(T) * N;
+            m_ArrayData[arrName]        = std::vector<unsigned char>(arraySize);
+        }
+        else
+        {
+            m_ArrayData[arrName].resize(arraySize);
+        }
+    }
+
+    void setNumParticles(unsigned int numParticles)
+    {
+        m_NumParticles = numParticles;
+    }
+
+    template<class T>
+    void setParticleRadius(T particleRadius)
+    {
+        m_ParticleRadius = static_cast<double>(particleRadius);
+    }
     //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
     unsigned int getNumParticles()
     {
         return m_NumParticles;
-    }
-    unsigned int getNumActiveParticles()
-    {
-        return m_NumActiveParticles;
     }
     unsigned int getMaxNumParticles()
     {
         return m_MaxNumParticles;
     }
 
-    bool hasData(std::string dataName)
+    template<class T>
+    T getParticleRadius()
     {
-        return (m_DataArrays.find(dataName) != m_DataArrays.end());
+        return static_cast<T>(m_ParticleRadius);
     }
 
-    std::vector<unsigned char>& getParticleData(std::string dataName)
+    unsigned int getUInt(std::string dataName)
     {
-        if(!hasData(dataName))
-        {
-            m_DataArrays[dataName] = std::vector<unsigned char>(0);
-        }
-
-        return m_DataArrays[dataName];
+        return hasScalar(dataName) ? m_ScalarData[dataName].uintValue : 0;
     }
 
+    unsigned int getDouble(std::string dataName)
+    {
+        return hasScalar(dataName) ? m_ScalarData[dataName].doubleValue : 0;
+    }
+
+    std::vector<unsigned char>& getArray(std::string arrName)
+    {
+        assert(hasArray(arrName));
+        return m_ArrayData[arrName];
+    }
+
+    //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 private:
+    double       m_ParticleRadius;
     unsigned int m_NumParticles;
-    unsigned int m_NumActiveParticles;
     unsigned int m_MaxNumParticles;
 
-    std::map<std::string, std::vector<unsigned char> > m_DataArrays;
+    std::map<std::string, ScalarValue>                 m_ScalarData;
+    std::map<std::string, std::vector<unsigned char> > m_ArrayData;
+    std::map<std::string, size_t>                      m_ArrayElementSize;
 };
