e6b094083a1375fdc751e86f75ea84f732c637f3
Nghia Truong <nghiatruong.vn@gmail.com>
2017-08-12 17:22:01 -0600

    add commandline argument parser :star:

---
+++ BananaCore/Include/Banana/Utils/CommandLineParser.h
@@ -0,0 +1,64 @@
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//
+//  Copyright (c) 2017 by
+//       __      _     _         _____
+//    /\ \ \__ _| |__ (_) __ _  /__   \_ __ _   _  ___  _ __   __ _
+//   /  \/ / _` | '_ \| |/ _` |   / /\/ '__| | | |/ _ \| '_ \ / _` |
+//  / /\  / (_| | | | | | (_| |  / /  | |  | |_| | (_) | | | | (_| |
+//  \_\ \/ \__, |_| |_|_|\__,_|  \/   |_|   \__,_|\___/|_| |_|\__, |
+//         |___/                                              |___/
+//
+//  <nghiatruong.vn@gmail.com>
+//  All rights reserved.
+//
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+#pragma once
+
+#include <map>
+#include <string>
+#include <algorithm>
+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+namespace Banana
+{
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+class CommandLineParser : public std::map<const std::string, const std::string>
+{
+public:
+    void init(int argc, char** argv)
+    {
+        for(int arg = 1; arg < argc; ++arg)
+        {
+            std::string argstr(argv[arg]);
+            argstr.erase(std::remove(argstr.begin(), argstr.end(), '-'), argstr.end());
+
+            if(argstr.find('=') == std::string::npos)
+                throw std::exception("Invalid stdin: variable assignment must be '...=value' without spacing");
+
+            const std::string tag(argstr.substr(0, argstr.find('=')));   // exception if = not found
+            const std::string val(argstr.substr(tag.size() + 1));
+
+            if(find(tag) == end())
+                insert(std::make_pair(tag, val));
+            else
+                throw std::exception("Duplicate command line tag encountered");
+        }
+    }
+
+    bool get(const std::string& tag, std::string& value)
+    {
+        auto it = find(tag);
+
+        if(it == end())
+            return false;
+
+        value = it->second;
+        return true;
+    }
+};
+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+} // end namespace Banana
