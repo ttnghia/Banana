d24c0a1b3ca053bb7443ca61e50f9ab4d0dd8c93
Nghia Truong <nghiatruong.vn@gmail.com>
2017-09-05 22:22:52 -0600

    :earth_asia: fix signal handling

--- BananaCore/Banana/Utils/Logger.cpp
+++ BananaCore/Banana/Utils/Logger.cpp
@@ -233,7 +233,6 @@ void Logger::initialize()
     }
 
     ////////////////////////////////////////////////////////////////////////////////
-    signal(SIGABRT, Logger::signalHandler);
     signal(SIGFPE,  Logger::signalHandler);
     signal(SIGINT,  Logger::signalHandler);
     signal(SIGSEGV, Logger::signalHandler);
@@ -319,10 +318,6 @@ void Logger::signalHandler(int signum)
 
     switch(signum)
     {
-        case SIGABRT:
-            s_MainFuncLogger->printWarning("Signal Abort");
-            break;
-
         case SIGFPE:
             s_MainFuncLogger->printWarning("Signal Floating-Point Exception");
             break;
@@ -347,7 +342,9 @@ void Logger::signalHandler(int signum)
     s_MainFuncLogger->printLog("Cleanup and exit program..........");
     shutdown();
 
-    exit(EXIT_SUCCESS);
+    // Raise sig abort, which is not handled by this function
+    // This will guarantee to terminate the application
+    raise(SIGABRT);
 }
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
