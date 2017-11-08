db9a5f954c65b3c0d3bc3b7d04ed5b03f4a879c2
Nghia Truong <nghiatruong.vn@gmail.com>
2017-11-08 12:26:23 -0700

    :deciduous_tree: update scene

--- Applications/QtApps/ParticleSimulation/Source/ParticleSolverQt.h
+++ Applications/QtApps/ParticleSimulation/Source/ParticleSolverQt.h
@@ -27,11 +27,11 @@
 #include <ParticleSolvers/Peridynamics/PeridynamicsSolver.h>
 
 //#define PARTICLE_SOLVER WCSPHSolver
-//#define PARTICLE_SOLVER FLIP3D_Solver
-//#define PARTICLE_SOLVER AFLIP3D_Solver
 //#define PARTICLE_SOLVER PIC3D_Solver
-#define PARTICLE_SOLVER MPM3D_Solver
+//#define PARTICLE_SOLVER FLIP3D_Solver
 //#define PARTICLE_SOLVER APIC3D_Solver
+//#define PARTICLE_SOLVER AFLIP3D_Solver
+#define PARTICLE_SOLVER MPM3D_Solver
 //#define PARTICLE_SOLVER PeridynamicsSolver
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
