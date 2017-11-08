67269493a6c22ab7a1c608ba09cfd3a90fcc2df6
Nghia Truong <nghiatruong.vn@gmail.com>
2017-11-08 11:53:40 -0700

    :evergreen_tree: small fixes

--- BananaCore/Banana/Macros.h
+++ BananaCore/Banana/Macros.h
@@ -44,7 +44,9 @@
 #ifdef __BANANA_WINDOWS__
 //  Exclude rarely-used stuff from Windows headers
 #   define WIN32_LEAN_AND_MEAN
-#   define NOMINMAX
+#   ifndef NOMINMAX
+#     define NOMINMAX
+#   endif
 #   define __func__ __FUNCTION__
 #   include <Windows.h>
 #   include <exception>
