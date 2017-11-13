d92a07a9c595192c60fc4b5d161988330d0509e1
Nghia Truong <nghiatruong.vn@gmail.com>
2017-11-12 21:37:38 -0700

    :seedling: fix glm

--- BananaCore/Banana/Setup.h
+++ BananaCore/Banana/Setup.h
@@ -40,8 +40,6 @@
 #include <glm/gtc/quaternion.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/norm.hpp>
-#include <glm/gtx/transform.hpp>
-#include <glm/gtx/string_cast.hpp>
 #include <glm/gtx/euler_angles.hpp>
 #include <glm/gtx/component_wise.hpp>
 
