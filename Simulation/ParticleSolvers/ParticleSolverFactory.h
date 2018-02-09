88331e4de025cdc309294a9e9fc6b73add7075c4
Nghia Truong <nghiatruong.vn@gmail.com>
2018-02-09 14:43:30 -0700

    :snake: improve factory class

--- Simulation/ParticleSolvers/ParticleSolverFactory.h
+++ Simulation/ParticleSolvers/ParticleSolverFactory.h
@@ -22,6 +22,7 @@
 #pragma once
 
 #include <Banana/Setup.h>
+#include <ParticleSolvers/ParticleSolver.h>
 #include <map>
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -35,22 +36,8 @@ public:
     ParticleSolverFactory() = delete;
     using PtrCreateMethod   = UniquePtr<ParticleSolver<N, RealType> >(*)();
     ////////////////////////////////////////////////////////////////////////////////
-    static bool registerSolver(const String& solverName, PtrCreateMethod funcCreate)
-    {
-        if(auto it = s_CreateMethods.find(solverName); it == s_CreateMethods.end()) {
-            s_CreateMethods[solverName] = funcCreate;
-            return true;
-        }
-        return false;
-    }
-
-    static SharedPtr<ParticleSolver<N, RealType> > createSolver(const String& solverName)
-    {
-        if(auto it = s_CreateMethods.find(name); it != s_CreateMethods.end()) {
-            return it->second(); // call the createFunc
-        }
-        return nullptr;
-    }
+    static bool                                    registerSolver(const String& solverName, const String& solverDescription, PtrCreateMethod funcCreate);
+    static SharedPtr<ParticleSolver<N, RealType> > createSolver(const String& solverName);
 
 private:
     static inline std::map<String, PtrCreateMethod> s_CreateMethods;
@@ -59,6 +46,36 @@ private:
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 using ParticleSolverFactory2D = ParticleSolverFactory<2, Real>;
 using ParticleSolverFactory3D = ParticleSolverFactory<3, Real>;
+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+template<Int N, class RealType>
+bool ParticleSolverFactory<N, RealType >::registerSolver(const String& solverName, const String& solverDescription, PtrCreateMethod funcCreate)
+{
+    if(auto it = s_CreateMethods.find(solverName); it == s_CreateMethods.end()) {
+        s_CreateMethods[solverName] = funcCreate;
+        return true;
+    }
+    return false;
+}
+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+template<Int N, class RealType>
+SharedPtr<ParticleSolver<N, RealType> > ParticleSolverFactory<N, RealType >::createSolver(const String& solverName)
+{
+    if(auto it = s_CreateMethods.find(name); it != s_CreateMethods.end()) {
+        return it->second();         // call the createFunc
+    }
+    return nullptr;
+}
+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+template<class Solver>
+class RegisteredInFactory
+{
+protected:
+    static inline bool s_bRegistered = ParticleSolverFactory<Solver::solverDimension(), Solver::SolverRealType>::registerSolver(Solver::getSolverName(),
+                                                                                                                                Solver::getCreateMethod());
+};
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 }; // end namespace Banana::ParticleSolvers
