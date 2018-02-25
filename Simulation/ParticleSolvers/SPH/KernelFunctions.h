b4625b763f62265eb1cb0a672157f82317661fa7
Nghia Truong <nghiatruong.vn@gmail.com>
2018-02-25 16:05:17 -0700

    :partly_sunny: rewrite kernel functions

--- Simulation/ParticleSolvers/SPH/KernelFunctions.h
+++ Simulation/ParticleSolvers/SPH/KernelFunctions.h
@@ -47,10 +47,15 @@ public:
     {
         m_radius = val;
         const RealType pi = static_cast<RealType>(M_PI);
-
-        const RealType h3 = m_radius * m_radius * m_radius;
-        m_k      = 8.0_f / (pi * h3);
-        m_l      = 48.0_f / (pi * h3);
+        const RealType h2 = m_radius * m_radius;
+        const RealType h3 = h2 * m_radius;
+        if constexpr(N == 2) {
+            m_k = RealType(80.0 / 7.0) / (pi * h2);
+            m_l = RealType(480.0 / 7.0) / (pi * h2);
+        } else {
+            m_k = RealType(8.0) / (pi * h3);
+            m_l = RealType(48.0) / (pi * h3);
+        }
         m_W_zero = W(VecX<N, RealType>(0));
     }
 
@@ -59,13 +64,13 @@ public:
     {
         RealType       res = 0;
         const RealType q   = r / m_radius;
-        if(q <= 1.0_f) {
-            if(q <= 0.5_f) {
+        if(q <= RealType(1.0)) {
+            if(q <= RealType(0.5)) {
                 const RealType q2 = q * q;
                 const RealType q3 = q2 * q;
-                res = m_k * (6.0_f * q3 - 6.0_f * q2 + 1.0_f);
+                res = m_k * (RealType(6.0) * q3 - RealType(6.0) * q2 + RealType(1.0));
             } else {
-                res = m_k * (2.0_f * pow(1.0_f - q, 3));
+                res = m_k * (RealType(2.0) * pow(RealType(1.0) - q, 3));
             }
         }
         return res;
@@ -81,12 +86,12 @@ public:
         VecX<N, RealType> res = VecX<N, RealType>(0);
         const RealType    rl  = glm::length(r);
         const RealType    q   = rl / m_radius;
-        if(q <= 1.0_f && rl > 1e-6_f) {
-            const VecX<N, RealType> gradq = r * (1.0_f / (rl * m_radius));
-            if(q <= 0.5_f) {
-                res = m_l * q * (3.0_f * q - 2.0_f) * gradq;
+        if(q <= RealType(1.0) && rl > RealType(1e-6)) {
+            const VecX<N, RealType> gradq = r * (RealType(1.0) / (rl * m_radius));
+            if(q <= RealType(0.5)) {
+                res = m_l * q * (RealType(3.0) * q - RealType(2.0)) * gradq;
             } else {
-                const RealType factor = 1.0_f - q;
+                const RealType factor = RealType(1.0) - q;
                 res = m_l * (-factor * factor) * gradq;
             }
         }
@@ -120,8 +125,13 @@ public:
     {
         m_radius = val;
         const RealType pi = static_cast<RealType>(M_PI);
-        m_k      = 315.0_f / (64.0_f * pi * pow(m_radius, 9));
-        m_l      = -945.0_f / (32.0_f * pi * pow(m_radius, 9));
+        if constexpr(N == 2) {
+            m_k = RealType(4.0) / (pi * pow(m_radius, 8));
+            m_l = -RealType(24.0) / (pi * pow(m_radius, 8));
+        } else {
+            m_k = RealType(315.0) / (RealType(64.0) * pi * pow(m_radius, 9));
+            m_l = -RealType(945.0) / (RealType(32.0) * pi * pow(m_radius, 9));
+        }
         m_m      = m_l;
         m_W_zero = W(VecX<N, RealType>(0));
     }
@@ -182,7 +192,7 @@ public:
         const RealType radius2 = m_radius * m_radius;
         if(r2 <= radius2) {
             RealType tmp  = radius2 - r2;
-            RealType tmp2 = 3.0_f * radius2 - 7.0_f * r2;
+            RealType tmp2 = RealType(3.0) * radius2 - RealType(7.0) * r2;
             res = m_m * tmp * tmp2;
         }
 
@@ -213,10 +223,16 @@ public:
     void setRadius(RealType val)
     {
         m_radius = val;
-        const RealType radius6 = pow(m_radius, 6);
-        const RealType pi      = RealType(M_PI);
-        m_k      = 15.0_f / (pi * radius6);
-        m_l      = -45.0_f / (pi * radius6);
+        const RealType pi = RealType(M_PI);
+        if constexpr(N == 2) {
+            const RealType radius5 = pow(m_radius, 5);
+            m_k = RealType(10.0) / (pi * radius5);
+            m_l = -RealType(30.0) / (pi * radius5);
+        } else {
+            const RealType radius6 = pow(m_radius, 6);
+            m_k = RealType(15.0) / (pi * radius6);
+            m_l = -RealType(45.0) / (pi * radius6);
+        }
         m_W_zero = W(VecX<N, RealType>(0));
     }
 
@@ -292,8 +308,12 @@ public:
     {
         m_radius = val;
         const RealType pi = static_cast<RealType>(M_PI);
-        m_k      = 32.0_f / (pi * pow(m_radius, 9));
-        m_c      = pow(m_radius, 6) / 64.0_f;
+        if constexpr(N == 2) {
+            __BNN_UNIMPLEMENTED_FUNC
+        } else {
+            m_k = RealType(32.0) / (pi * pow(m_radius, 9));
+            m_c = pow(m_radius, 6) / RealType(64.0);
+        }
         m_W_zero = W(VecX<N, RealType>(0));
     }
 
@@ -311,10 +331,10 @@ public:
         if(r2 <= radius2) {
             const RealType r1 = sqrt(r2);
             const RealType r3 = r2 * r1;
-            if(r1 > 0.5_f * m_radius) {
+            if(r1 > RealType(0.5) * m_radius) {
                 res = m_k * pow(m_radius - r1, 3) * r3;
             } else {
-                res = m_k * 2.0_f * pow(m_radius - r1, 3) * r3 - m_c;
+                res = m_k * RealType(2.0) * pow(m_radius - r1, 3) * r3 - m_c;
             }
         }
         return res;
@@ -328,10 +348,10 @@ public:
         if(r2 <= radius2) {
             const RealType r1 = sqrt(r2);
             const RealType r3 = r2 * r1;
-            if(r1 > 0.5_f * m_radius) {
+            if(r1 > RealType(0.5) * m_radius) {
                 res = m_k * pow(m_radius - r1, 3) * r3;
             } else {
-                res = m_k * 2.0_f * pow(m_radius - r1, 3) * r3 - m_c;
+                res = m_k * RealType(2.0) * pow(m_radius - r1, 3) * r3 - m_c;
             }
         }
         return res;
@@ -360,7 +380,11 @@ public:
     void setRadius(RealType val)
     {
         m_radius = val;
-        m_k      = 0.007_f / pow(m_radius, 3.25_f);
+        if constexpr(N == 2) {
+            __BNN_UNIMPLEMENTED_FUNC
+        } else {
+            m_k = 0.007_f / pow(m_radius, 3.25_f);
+        }
         m_W_zero = W(VecX<N, RealType>(0));
     }
 
@@ -376,8 +400,8 @@ public:
         const RealType radius2 = m_radius * m_radius;
         if(r2 <= radius2) {
             const RealType r = sqrt(r2);
-            if(r > 0.5_f * m_radius) {
-                res = m_k * pow(-4.0_f * r2 / m_radius + 6.0_f * r - 2.0_f * m_radius, 0.25_f);
+            if(r > RealType(0.5) * m_radius) {
+                res = m_k * pow(-4.0_f * r2 / m_radius + RealType(6.0) * r - RealType(2.0) * m_radius, 0.25_f);
             }
         }
         return res;
@@ -390,8 +414,8 @@ public:
         const RealType radius2 = m_radius * m_radius;
         if(r2 <= radius2) {
             const RealType r = sqrt(r2);
-            if(r > 0.5_f * m_radius) {
-                res = m_k * pow(-4.0_f * r2 / m_radius + 6.0_f * r - 2.0_f * m_radius, 0.25_f);
+            if(r > RealType(0.5) * m_radius) {
+                res = m_k * pow(-4.0_f * r2 / m_radius + RealType(6.0) * r - RealType(2.0) * m_radius, 0.25_f);
             }
         }
         return res;
@@ -404,7 +428,7 @@ public:
 };
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-template<Int N, class RealType, template<Int, class> class KernelType, unsigned int resolution = 1000u>
+template<Int N, class RealType, template<Int, class> class KernelType, UInt resolution = 1000u>
 class PrecomputedKernel
 {
 protected:
@@ -428,11 +452,11 @@ public:
         m_radius2 = val * val;
         kernel.setRadius(val);
         const RealType stepSize = m_radius / (RealType)resolution;
-        m_invStepSize = 1.0_f / stepSize;
-        for(unsigned int i = 0; i < resolution; i++) {
-            const RealType posX = stepSize * (RealType)i;               // Store kernel values in the middle of an interval
+        m_invStepSize = RealType(1.0) / stepSize;
+        for(UInt i = 0; i < resolution; i++) {
+            const RealType posX = stepSize * static_cast<RealType>(i);               // Store kernel values in the middle of an interval
             m_W[i] = kernel.W(posX);
-            if(posX > 1e-6_f) {
+            if(posX > RealType(1e-6)) {
                 if constexpr(N == 2) {
                     m_gradW[i] = kernel.gradW(VecX<N, RealType>(posX, 0))[0] / posX;
                 } else {
@@ -452,8 +476,7 @@ public:
         RealType       res = 0;
         const RealType r2  = glm::length2(r);
         if(r2 <= m_radius2) {
-            const RealType     r   = sqrt(r2);
-            const unsigned int pos = std::min<unsigned int>((unsigned int)(r * m_invStepSize), resolution);
+            const UInt pos = std::min<UInt>(static_cast<UInt>(sqrt(r2) * m_invStepSize), resolution);
             res = m_W[pos];
         }
         return res;
@@ -463,7 +486,7 @@ public:
     {
         RealType res = 0;
         if(r <= m_radius) {
-            const unsigned int pos = std::min<unsigned int>((unsigned int)(r * m_invStepSize), resolution);
+            const UInt pos = std::min<UInt>(static_cast<UInt>(r * m_invStepSize), resolution);
             res = m_W[pos];
         }
         return res;
@@ -474,8 +497,8 @@ public:
         VecX<N, RealType> res = VecX<N, RealType>(0);
         const RealType    r2  = glm::length2(r);
         if(r2 <= m_radius2) {
-            const RealType     rl  = sqrt(r2);
-            const unsigned int pos = std::min<unsigned int>((unsigned int)(rl * m_invStepSize), resolution);
+            const RealType rl  = sqrt(r2);
+            const UInt     pos = std::min<UInt>(static_cast<UInt>(rl * m_invStepSize), resolution);
             res = m_gradW[pos] * r;
         }
 
