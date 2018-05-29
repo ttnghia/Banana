e7aa3482406f2377ddb90a54dc797b7718b56459
Nghia Truong <nghiatruong.vn@gmail.com>
2018-05-28 23:18:56 -0600

    :small_red_triangle: add ScopeTimer

--- BananaCore/Banana/Utils/Timer.h
+++ BananaCore/Banana/Utils/Timer.h
@@ -26,6 +26,7 @@
 #include <cassert>
 #include <sstream>
 #include <iomanip>
+#include <functional>
 
 #include <Banana/Utils/NumberHelpers.h>
 
@@ -35,9 +36,12 @@ namespace Banana
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 class Timer
 {
+protected:
     using Clock = std::chrono::high_resolution_clock;
+
 public:
-    Timer() : m_TimerTicked(false) {}
+    Timer()          = default;
+    virtual ~Timer() = default;
 
     void tick()
     {
@@ -61,15 +65,13 @@ public:
         if(m_TimerTicked) {
             tock();
         }
-
         m_StrBuilder.str("");
         m_StrBuilder << NumberHelpers::formatWithCommas(m_ElapsedTime);
         m_StrBuilder << "ms";
-
         return m_StrBuilder.str();
     }
 
-    String getRunTime(const char* caption)
+    String getRunTime(const String& caption)
     {
         if(m_TimerTicked) {
             tock();
@@ -77,6 +79,7 @@ public:
 
         m_StrBuilder.str("");
         m_StrBuilder << caption;
+        m_StrBuilder << ": ";
         m_StrBuilder << NumberHelpers::formatWithCommas(m_ElapsedTime);
         m_StrBuilder << "ms";
 
@@ -84,16 +87,14 @@ public:
     }
 
     template<class Function>
-    static String getRunTime(const char* caption, const Function& function)
+    static String getRunTime(const String& caption, const Function& function)
     {
         Timer timer;
-
         ////////////////////////////////////////////////////////////////////////////////
         timer.tick();
         function();
         timer.tock();
         ////////////////////////////////////////////////////////////////////////////////
-
         return timer.getRunTime(caption);
     }
 
@@ -101,9 +102,30 @@ private:
     Clock::time_point m_StartTime;
     Clock::time_point m_EndTime;
     std::stringstream m_StrBuilder;
-    double            m_ElapsedTime;
-    bool              m_TimerTicked;
+
+    double m_ElapsedTime { 0.0 };
+    bool   m_TimerTicked { false };
 };
 
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+class ScopeTimer : public Timer
+{
+public:
+    ScopeTimer(const std::function<void(const String&)>& exitFunc) : m_ExitFunc(exitFunc), m_bDefaultCaption(false) { this->tick(); }
+    ScopeTimer(const String& caption) : m_Caption(caption), m_bDefaultCaption(true) { this->tick(); }
+    ~ScopeTimer()
+    {
+        if(m_bDefaultCaption) {
+            printf("%s\n", this->getRunTime(m_Caption).c_str());
+        } else {
+            m_ExitFunc(this->getRunTime());
+        }
+    }
+
+protected:
+    std::function<void(const String&)> m_ExitFunc;
+    String                             m_Caption;
+    bool                               m_bDefaultCaption;
+};
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 } // end namespace Banana
