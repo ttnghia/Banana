d8804679974fbf52c6f3956b8bf41babad607a0f
Nghia Truong <nghiatruong.vn@gmail.com>
2018-06-06 22:33:43 -0600

    :notes: remove toString and related functions from NumberHelpers

--- BananaCore/Banana/Utils/NumberHelpers.h
+++ BananaCore/Banana/Utils/NumberHelpers.h
@@ -40,14 +40,14 @@ namespace NumberHelpers
 {
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 template<class T>
-inline bool isValidNumber(T x)
+bool isValidNumber(T x)
 {
     return !std::isnan(x) && !std::isinf(x);
 }
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 template<class IndexType, Int N, class RealType>
-inline VecX<N, IndexType> createGrid(const VecX<N, RealType>& bmin, const VecX<N, RealType>& bmax, RealType spacing)
+VecX<N, IndexType> createGrid(const VecX<N, RealType>& bmin, const VecX<N, RealType>& bmax, RealType spacing)
 {
     VecX<N, RealType>  fgrid = (bmax - bmin) / spacing;
     VecX<N, IndexType> result;
@@ -61,7 +61,7 @@ inline VecX<N, IndexType> createGrid(const VecX<N, RealType>& bmin, const VecX<N
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 template<Int N, class IndexType, class Function>
-inline void scan(VecX<N, IndexType> idx, const VecX<N, IndexType>& minIdx, const VecX<N, IndexType>& maxIdx, Function&& f, Int dim = 0)
+void scan(VecX<N, IndexType> idx, const VecX<N, IndexType>& minIdx, const VecX<N, IndexType>& maxIdx, Function&& f, Int dim = 0)
 {
     if(dim == N - 1) {
         for(IndexType i = minIdx[dim]; i < maxIdx[dim]; ++i) {
@@ -77,29 +77,29 @@ inline void scan(VecX<N, IndexType> idx, const VecX<N, IndexType>& minIdx, const
 }
 
 template<Int N, class IndexType, class Function>
-inline void scan(const VecX<N, IndexType>& minIdx, const VecX<N, IndexType>& maxIdx, Function&& f, Int dim = 0)
+void scan(const VecX<N, IndexType>& minIdx, const VecX<N, IndexType>& maxIdx, Function&& f, Int dim = 0)
 {
     scan(VecX<N, IndexType>(0), minIdx, maxIdx, std::forward<Function>(f), dim);
 }
 
 template<Int N, class IndexType, class Function>
-inline void scan(const VecX<N, IndexType>& maxIdx, Function&& f, Int dim = 0)
+void scan(const VecX<N, IndexType>& maxIdx, Function&& f, Int dim = 0)
 {
     scan(VecX<N, IndexType>(0), VecX<N, IndexType>(0), maxIdx, std::forward<Function>(f), dim);
 }
 
 template<Int N, class IndexType, class Function>
-inline void scan11(Function&& f)
+void scan11(Function&& f)
 {
     scan(VecX<N, IndexType>(0), VecX<N, IndexType>(-1), VecX<N, IndexType>(2), std::forward<Function>(f));
 }
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 template<class IntType>
-inline IntType generateRandomInt(IntType start = 0, IntType end = std::numeric_limits<IntType>::max())
+IntType generateRandomInt(IntType start = 0, IntType end = std::numeric_limits<IntType>::max())
 {
     if(end < start) {
-        end = start;
+        std::swap(end, start);
     }
     std::random_device                     rd;
     std::mt19937                           gen(rd());
@@ -108,17 +108,51 @@ inline IntType generateRandomInt(IntType start = 0, IntType end = std::numeric_l
     return dis(gen);
 }
 
-template<class RealType>
-inline RealType generateRandomReal(RealType start = RealType(0), RealType end = std::numeric_limits<RealType>::max())
+template<class IntType, class SizeType>
+Vector<IntType> generateRandomIntVector(SizeType size, IntType start = 0, IntType end = std::numeric_limits<IntType>::max())
 {
     if(end < start) {
-        end = start;
+        std::swap(end, start);
+    }
+    std::random_device                     rd;
+    std::mt19937                           gen(rd());
+    std::uniform_int_distribution<IntType> dis(start, end);
+
+    Vector<IntType> v(size);
+    for(SizeType i = 0; i < size; ++i) {
+        v[i] = dis(gen);
+    }
+    return v;
+}
+
+template<class RealType>
+RealType generateRandomReal(RealType start = RealType(0), RealType end = std::numeric_limits<RealType>::max())
+{
+    if(end < start) {
+        std::swap(end, start);
     }
     std::random_device                       rd;
     std::mt19937                             gen(rd());
     std::uniform_real_distribution<RealType> dis(start, end);
 
     return dis(gen);
+}
+
+template<class RealType, class SizeType>
+Vector<RealType> generateRandomRealVector(SizeType size, RealType start = RealType(0), RealType end = std::numeric_limits<RealType>::max())
+{
+    if(end < start) {
+        std::swap(end, start);
+    }
+    std::random_device                       rd;
+    std::mt19937                             gen(rd());
+    std::uniform_real_distribution<RealType> dis(start, end);
+
+    Vector<RealType> v(size);
+    for(SizeType i = 0; i < size; ++i) {
+        v[i] = dis(gen);
+    }
+    return v;
 }
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -140,136 +174,6 @@ private:
 };
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-template<class RealType>
-String formatWithCommas(RealType value, Int precision = 2)
-{
-    struct Numpunct : public std::numpunct<char>
-    {
-protected:
-        virtual char do_thousands_sep() const
-        {
-            return ',';
-        }
-
-        virtual String do_grouping() const
-        {
-            return "\03";
-        }
-    };
-
-    std::stringstream ss;
-    ss.str("");
-    ss.imbue({ std::locale(), new Numpunct });
-    ss << std::setprecision(precision) << std::fixed << value;
-    return ss.str();
-}
-
-//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-template<class RealType>
-String formatToScientific(RealType value, Int precision = 5)
-{
-    std::stringstream ss;
-    ss.str("");
-    ss << std::setprecision(precision) << std::fixed << std::scientific << value;
-    return ss.str();
-}
-
-//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-template<class RealType>
-inline String formatToScientific(const Vec3<RealType>& vec, Int precision = 5)
-{
-    std::stringstream ss;
-    ss.str("");
-
-    ss << String("[");
-    ss << std::setprecision(precision) << std::fixed << std::scientific << vec[0] + String(", ");
-    ss << std::setprecision(precision) << std::fixed << std::scientific << vec[1] + String(", ");
-    ss << std::setprecision(precision) << std::fixed << std::scientific << vec[2] + String("]");
-
-    return ss.str();
-}
-
-//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-inline String byteToHex(const char* arrBytes, size_t arrSize, Int width /*= 2*/)
-{
-    if(width < 2) { width = 2; }
-    std::stringstream ss;
-
-    for(size_t i = 0; i < arrSize; ++i) {
-        ss << std::hex << std::uppercase << std::setw(width) << static_cast<Int>(arrBytes[i]);
-    }
-
-    return ss.str();
-}
-
-template<class T>
-inline String byteToHex(const Vector<T>& vecBytes, Int width /*= 2*/)
-{
-    return byteToHex((const char*)vecBytes.data(), vecBytes.size() * sizeof(T), width);
-}
-
-//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-template<typename RealType>
-String toString(const RealType value, const Int precision = 5)
-{
-    std::ostringstream ss;
-    ss << std::setprecision(precision) << value;
-    return ss.str();
-}
-
-//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-template<Int N, class RealType>
-inline String toString(const VecX<N, RealType>& vec, Int precision = 5)
-{
-    std::stringstream ss;
-    ss.str("");
-
-    ss << String("[");
-
-    for(Int i = 0; i < N - 1; ++i) {
-        ss << formatWithCommas(vec[i], precision) << String(", ");
-    }
-    ss << formatWithCommas(vec[N - 1], precision) << String("]");
-
-    return ss.str();
-}
-
-//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-template<Int N, class RealType>
-inline String toString(const MatXxX<N, RealType>& mat, bool breakLine = false, Int precision = 5)
-{
-    std::stringstream ss;
-    ss.str("");
-
-    ss << String("[");
-
-    // N-1 line
-    for(Int i = 0; i < N - 1; ++i) {
-        for(Int j = 0; j < N - 1; ++j) {
-            ss << formatWithCommas(mat[j][i], precision) << String(", ");
-        }
-        ss << formatWithCommas(mat[N - 1][i], precision);
-
-        if(breakLine) {
-            ss << String(",") << std::endl;
-        } else {
-            ss << String("; ");
-        }
-    }
-
-    // last line
-    {
-        for(Int j = 0; j < N - 1; ++j) {
-            ss << formatWithCommas(mat[j][N - 1], precision) << String(", ");
-        }
-        ss << formatWithCommas(mat[N - 1][N - 1], precision);
-    }
-    ss << String("]");
-
-    return ss.str();
-}
-
-//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 template<Int N, class RealType>
 bool isInside(const VecX<N, RealType>& ppos, const VecX<N, RealType>& bMin, const VecX<N, RealType>& bMax)
 {
@@ -278,7 +182,6 @@ bool isInside(const VecX<N, RealType>& ppos, const VecX<N, RealType>& bMin, cons
             return false;
         }
     }
-
     return true;
 }
 
@@ -288,15 +191,6 @@ void jitter(VecX<N, RealType1>& ppos, RealType2 maxJitter)
 {
     for(Int j = 0; j < N; ++j) {
         ppos += MathHelpers::frand11<RealType1>() * static_cast<RealType1>(maxJitter);
-    }
-}
-
-//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-template<Int N, class RealType>
-void clamp(VecX<N, RealType>& ppos, const VecX<N, RealType>& bMin, const VecX<N, RealType>& bMax, Real margin /*= 0*/)
-{
-    for(UInt i = 0; i < N; ++i) {
-        ppos[i] = MathHelpers::clamp(ppos[i], bMin[i] + margin, bMax[i] - margin);
     }
 }
 
@@ -353,9 +247,9 @@ void rotate(Vec_VecX<N, RealType>& points, const VecX<N, RealType>& rotation)
     Scheduler::parallel_for(points.size(), [&](size_t i)
                             {
                                 const auto& pi = points[i];
-                                points[i] = Vec3<RealType>(glm::dot(R[0], pi),
-                                                           glm::dot(R[1], pi),
-                                                           glm::dot(R[2], pi));
+                                points[i]      = Vec3<RealType>(glm::dot(R[0], pi),
+                                                                glm::dot(R[1], pi),
+                                                                glm::dot(R[2], pi));
                             });
 }
 
